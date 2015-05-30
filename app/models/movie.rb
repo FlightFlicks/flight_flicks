d41acf63259d0d96accb7cb2113ffc0f61cb9f1c
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-05-30 10:36:33 -0400

    Key not giving access anymore

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,21 +8,24 @@ class Movie
   end
 
   def get_data
-    HTTParty.get("http://api.themoviedb.org/3/movie/#{movie_id}?api_key=#{ENV[“MOVIE_KEY”]}")
+    HTTParty.get("http://api.themoviedb.org/3/movie/#{@movie_id}?api_key=#{ENV["MOVIE_KEY"]}")
     #will change later
   end
 
   def movie_run_time
-    Flight.duration.to_i
+    @page[runtime] < Flight.duration.to_i
+    @movies.each do |movie_id|
+      movie.
   end
 
   def movie_title
     @page["title"]
   end
 
-  # def movie_id
-  #   @movie_id = MovieGenre.
-  # end
+  def movie_id
+    @movies.each do
+    @movie_id = MovieGenre.find_movie
+  end
 
 
 
--- app/models/movie_genre.rb
+++ app/models/movie_genre.rb
@@ -1,12 +1,18 @@
 class MovieGenre
 
-  def initialize(genre)
+  def initialize
     @page = get_data
-    @genre = genre
-
+    @movies = []
   end
 
   def get_data
-    HTTParty.get("http://api.themoviedb.org/3/genre/12/movies?api_key=#{ENV[“MOVIE_KEY”]}&page=#{(1..2000).sample}")
+    HTTParty.get("http://api.themoviedb.org/3/genre/#{Genre.get_id}/movies?api_key=#{ENV["MOVIE_KEY"]}&page=#{(1..2000).sample}")
     #will change later
+  end
+
+  def find_movie
+    @page["id"]["page"]["results"].each do |movie|
+      movies << movie["id"]
+
+    end
   end

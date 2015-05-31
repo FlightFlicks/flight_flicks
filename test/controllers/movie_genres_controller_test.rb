6bcd8338968bb776fb332a70d68380e35f03f778
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-05-31 19:54:36 -0400

    get_id not working

--- test/controllers/movie_genres_controller_test.rb
+++ test/controllers/movie_genres_controller_test.rb
@@ -3,6 +3,12 @@ require 'test_helper'
 class MovieGenresControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
-    assert_response :success
+    assert response.body :success
+  end
+
+  test "should show flight duration" do
+    flight = Flight.new("Raleigh", "Denver")
+    get :show
+    assert response.body =~ /220/
   end
 end

7bfaf776c33ebdf499337284af3bc8b430af43ae
Zack Strickland <zas0923@gmail.com>
2015-05-31 15:42:39 -0400

    still working on show test in flight controller...

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,7 +5,7 @@ class FlightsController < ApplicationController
 
   def show
     f = Flight.new(params[:origin], params[:destination])
-    render json: {:origin => params[:origin], :destination => params[:destination], :flight_time => f.duration}
+    render json: f.duration
     #not sure if this is right...
   end
 

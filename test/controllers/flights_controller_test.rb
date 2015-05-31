0d53f3127257273e7b3a4f41e586e976fb6a35aa
Zack Strickland <zas0923@gmail.com>
2015-05-31 15:33:59 -0400

    add flight controller show test. still failing.

--- test/controllers/flights_controller_test.rb
+++ test/controllers/flights_controller_test.rb
@@ -6,6 +6,12 @@ class FlightsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should show flight duration" do
+    flight = Flight.new("Raleigh", "Dallas")
+    page = get :show
+    assert_equal 185, page["flight_time"]
+  end
+
 
 
 end
